b13c83ada46f8185ee99654400cab809077fb2de
Saleem Saleh <salem1234saleem@gmail.com>
2021-02-13 14:25:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,14 +20,25 @@ function mapToSquare(sourceArray) {
 function reduceToTotal(sourceArray, startingPoint = 0) {
 const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
+<<<<<<< HEAD
   return  startingPoint +  sourceArray.reduce(reducer);
+=======
+  return    sourceArray.reduce(reducer,startingPoint);
+>>>>>>> c74025c16055dd35a1297bf7416420110272e605
 
 }
 
 function reduceToAllTrue(sourceArray) {
+<<<<<<< HEAD
     return sourceArray.reduce((acc, val) => acc && Boolean(val), true);
 }
 
 function reduceToAnyTrue(sourceArray) {
     return sourceArray.reduce((acc, val) => acc || Boolean(val));
+=======
+const reducer = (accumulator, currentValue) => return accumulator + currentValue;
+
+  return   sourceArray.reduce(reducer);
+
+>>>>>>> c74025c16055dd35a1297bf7416420110272e605
 }
